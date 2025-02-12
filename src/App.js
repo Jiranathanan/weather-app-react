e5a25324b6a7c3d0d6fe66e94346de5072c3fa97
Jiranathanan Siripun <jiranathanan@gmail.com>
2025-02-12 15:27:38 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+    <h1>Test Successful</h1>
+    <button onClick={() => console.log("Click!!!!!")}>Click for more</button>
+    </div>
+  )
+}
+
+
+export default App;
