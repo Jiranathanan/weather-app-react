1c49be01b21ec1fa1594a79dd027e2f19e370c93
Jiranathanan Siripun <jiranathanan@gmail.com>
2025-02-14 23:21:56 +0700

    Add Hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [allData, setAllData] = useState({
+    city: '',
+    country: '',
+    temperature: ''
+  })
+
+  useEffect( () => {
+    // what we want to happen after rendering
+    // fetch the database information the API call of weather
+    
+  }, [])
+
   return (
     <div className='App'>
     <h1>Test Successful</h1>
