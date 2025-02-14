8621efa82acd841d633af50125fb46dc235b3a11
Jiranathanan Siripun <jiranathanan@gmail.com>
2025-02-15 00:01:55 +0700

    Add api key in .env, don't forget to name it start with REACT_APP_

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [allData, setAllData] = useState({
-    city: '',
-    country: '',
-    temperature: ''
-  })
+  console.log(process.env.REACT_APP_WEATHER_API)
+  // const [allData, setAllData] = useState({
+  //   city: '',
+  //   country: '',
+  //   temperature: ''
+  // })
 
   useEffect( () => {
     // what we want to happen after rendering
     // fetch the database information the API call of weather
-    
+
   }, [])
 
   return (
     <div className='App'>
     <h1>Test Successful</h1>
+    <h2>API: {process.env.REACT_APP_WEATHER_API}</h2>
     <button onClick={() => console.log("Click!!!!!")}>Click for more</button>
     </div>
   )
