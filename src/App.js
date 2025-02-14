36553456b8e95d73ddf2f04100d29514925b0c38
Jiranathanan Siripun <jiranathanan@gmail.com>
2025-02-15 00:49:07 +0700

    Test fetching data

--- src/App.js
+++ src/App.js
@@ -13,12 +13,19 @@ function App() {
   useEffect( () => {
     // what we want to happen after rendering
     // fetch the database information the API call of weather
-
+    fetchData('london');
   }, [])
 
   const fetchData = async(city) => {
     const APIKEY = process.env.REACT_APP_WEATHER_API
     // axios is a library that makes request to the backend
+    const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}`)
+    console.log(result)
+    setAllData({
+      city: result.data.name,
+      country: result.data.sys.country,
+      temperature: result.data.main.temp 
+    })
 
   }
 
@@ -27,6 +34,9 @@ function App() {
     <h1>Test Successful</h1>
     <h2>API: {process.env.REACT_APP_WEATHER_API}</h2>
     <button onClick={() => console.log("Click!!!!!")}>Click for more</button>
+    <h3>City: {allData.city}</h3>
+    <h3>Country: {allData.country}</h3>
+    <h2>Temp: {allData.temperature} F</h2>
     </div>
   )
 }
