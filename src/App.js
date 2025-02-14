6806836c40b8d4d63402c50bcac7cb95fe2fabec
Jiranathanan Siripun <jiranathanan@gmail.com>
2025-02-15 00:18:21 +0700

    Edit README.md

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
   console.log(process.env.REACT_APP_WEATHER_API)
-  // const [allData, setAllData] = useState({
-  //   city: '',
-  //   country: '',
-  //   temperature: ''
-  // })
+  const [allData, setAllData] = useState({
+    city: '',
+    country: '',
+    temperature: ''
+  })
 
   useEffect( () => {
     // what we want to happen after rendering
     // fetch the database information the API call of weather
 
   }, [])
+
+  const fetchData = async(city) => {
+    const APIKEY = process.env.REACT_APP_WEATHER_API
+    // axios is a library that makes request to the backend
+
+  }
 
   return (
     <div className='App'>
