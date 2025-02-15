6d778edc885d2f4a80b9d447a570d8c2f8414e93
Jiranathanan Siripun <jiranathanan@gmail.com>
2025-02-16 02:52:36 +0700

    Add preventDefault

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
-  console.log(process.env.REACT_APP_WEATHER_API)
+  // console.log(process.env.REACT_APP_WEATHER_API)
+  const [search, setSearch] = useState('')
   const [allData, setAllData] = useState({
     city: '',
     country: '',
@@ -22,7 +23,7 @@ function App() {
       const APIKEY = process.env.REACT_APP_WEATHER_API
       // axios is a library that makes request to the backend
       const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`)
-      console.log(result)
+      // console.log(result)
       setAllData({
         city: result.data.name,
         country: result.data.sys.country,
@@ -33,15 +34,32 @@ function App() {
     }
   }
 
+  const handleSubmit = (e) => {
+    console.log(search)
+    e.preventDefault()
+    fetchData(search)
+  }
+
+  const handleChange = (e) => {
+    // console.log('this is listening')
+    setSearch(e.target.value)
+    // console.log(search)
+  }
+
   return (
     <main>
     <div className='App'>
-      <h1>Test Successful</h1>
-      <h2>API: {process.env.REACT_APP_WEATHER_API}</h2>
-      <button onClick={() => console.log("Click!!!!!")}>Click for more</button>
-      <h3>City: {allData.city}</h3>
-      <h3>Country: {allData.country}</h3>
-      <h2>Temp: {allData.temperature}° Celcius</h2>
+      <h1>Enter City Name</h1>
+      <form onSubmit={handleSubmit}>
+        <input value={search} type='text' name='city' placeholder='city name' onChange={handleChange} />
+        <button style={{marginLeft: 5}}>Search</button>
+      </form>
+      <section>
+        {/* <h2>API: {process.env.REACT_APP_WEATHER_API}</h2> */}
+        <h3>City: {allData.city}</h3>
+        <h3>Country: {allData.country}</h3>
+        <h2>Temp: {allData.temperature}° Celcius</h2>
+      </section>
     </div>
    </main>
   )
